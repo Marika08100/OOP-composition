eafa80c83be09c0cb312eed3f15713300e671359
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-11 17:02:42 +0300

    is rdy

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,68 @@
 public class Book {
+    private String name;
+    private Author author;
+    private double price;
+    private int qty = 0;
+
+    public Book(String name, Author author, double price){
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+    public Book(String name, Author author, double price, int qty){
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+    @Override
+    public String toString() {
+        return "Book[name=" + name + ", Author[" + author.toString() + "], price=" + price + ", qty=" + qty + "]";
+    }
+    public String getAuthorName() {
+        return author.getName();
+    }
+
+    public String getAuthorEmail() {
+        return author.getEmail();
+    }
+
+    public char getAuthorGender() {
+        return author.getGender();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+
 }
+
--- src/TestAuthor.java
+++ src/TestAuthor.java
@@ -1,2 +1,20 @@
 public class TestAuthor {
+
+    static Author ahTeck = new Author("Tan Ah Teck", "ahteck@nowhere.com", 'm'); // Test the constructor
+
+    /**
+     * Constructs a new object.
+     */
+    public TestAuthor() {
+    }
+
+    public static void main(String[] args) {
+
+
+        System.out.println(ahTeck.toString());  // Test toString()
+        ahTeck.setEmail("paulTan@nowhere.com");  // Test setter
+        System.out.println("name is: " + ahTeck.getName());     // Test getter
+        System.out.println("eamil is: " + ahTeck.getEmail());   // Test getter
+        System.out.println("gender is: " + ahTeck.getGender()); // Test gExerciseOOP_MyPolynomial.pngetter
+    }
 }
